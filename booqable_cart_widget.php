3ae231208393b6c147d5e5a5a2babdd30043ed03
Johan van Zonneveld <johan@vzonneveld.nl>
2015-07-14 13:07:05 +0200

    Add checkout url option

--- booqable_cart_widget.php
+++ booqable_cart_widget.php
@@ -10,17 +10,25 @@
   	}
 
   	public function widget( $args, $instance ) {
-      echo '<div id="booqable-cart-component" class="widget"></div>';
+      echo '<div id="booqable-cart-component" class="widget" data-checkout-url="' . $instance['checkout_url'] . '"></div>';
   	}
 
   	public function form( $instance ) {
+      $checkout_url = ! empty( $instance['checkout_url'] ) ? $instance['checkout_url'] : __( '', 'text_domain' );
   		?>
-  		<p></p>
+  		<p>
+  		<label for="<?php echo $this->get_field_id( 'checkout_url' ); ?>"><?php _e( 'Chekcout url:' ); ?></label>
+  		<input class="widefat" id="<?php echo $this->get_field_id( 'checkout_url' ); ?>" name="<?php echo $this->get_field_name( 'checkout_url' ); ?>" type="text" value="<?php echo esc_attr( $checkout_url ); ?>">
+  		</p>
+      <p class="description">Leave blank to use the Booqable hosted checkout</p>
   		<?php
   	}
 
   	public function update( $new_instance, $old_instance ) {
-      array();
+      $instance = array();
+		  $instance['checkout_url'] = ( ! empty( $new_instance['checkout_url'] ) ) ? strip_tags( $new_instance['checkout_url'] ) : '';
+
+		  return $instance;
   	}
   }
 
